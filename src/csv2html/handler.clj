5a229a4f5e1a03f0533d8f78a35f300aae69f555
Bastien <bzg@gnu.org>
2018-10-03 18:14:10 +0200

    Add the dummiest captch ever

--- src/csv2html/handler.clj
+++ src/csv2html/handler.clj
@@ -16,27 +16,35 @@
             [clojure.data.csv :as data-csv]
             [csv2html.config :as config]))
 
+(def dummy-captcha
+  ["35 + 7" "32 + 10" "84 / 2" "21 * 2" "44 - 2"
+   "36 + 6" "31 + 11" "126 / 3" "40 + 2" "46 - 4"
+   "29 + 12" "12 + 30" "9 + 33" "14 * 3" "17 + 25"])
+
 (defn- home-page []
   (h/html5
    {:lang "fr"}
    [:head
-    [:title "csv2html - export a .csv file as a HTML page"]
+    [:title "csv2html - convert a .csv file as a HTML page"]
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     (h/include-css
      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")]
    [:body
-    [:div {:class "container"}
-     [:h1 "Export a .csv file to a HTML page"]
+    [:div {:class "container" :style "width:80%"}
+     [:h1 "Convert a .csv file to a HTML page"]
      [:form
       {:action "/file" :method "post" :enctype "multipart/form-data"}
+      [:div {:class "form-group"}
+       [:p (str (rand-nth dummy-captcha) " = ")
+        [:input {:name "test-field" :type "text" :size "3" :required true}]]]
       [:div {:class "form-group"}
        [:input {:name   "file" :type "file" :size "200" :required true
                 :accept "application/csv"}]]
       [:div {:class "form-group"}
-       [:input {:type "submit" :value "Export" :class "btn btn-warning btn-lg"}]]]]]))
+       [:input {:type "submit" :value "Convert" :class "btn btn-warning btn-lg"}]]]]]))
 
-(defn- to-html [filename csv-data]
+(defn- to-html [test-field filename csv-data]
   (h/html5
    {:lang "fr"}
    [:head
@@ -60,24 +68,27 @@
     [:script
      "$(function(){$(\"#example\").dataTable({\"oLanguage\":{\"sUrl\":\"http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json\",},\"iDisplayLength\": 15});})"]]))
 
-(defn- handle-upload [actual-file dest-file dest-dir filename]
+(defn- handle-upload [test-field actual-file dest-file dest-dir filename]
   (.mkdir (io/file dest-dir))
   (io/copy actual-file (io/file dest-file))
   (spit (str dest-dir "index.html")
-        (to-html filename
+        (to-html test-field
+                 filename
                  (data-csv/read-csv (slurp actual-file)))))
 
 (defroutes app-routes
   (GET "/" [] (home-page))
-  (POST "/file" [file]
+  (POST "/file" [file test-field]
         (let [file-name   (:filename file)
               actual-file (:tempfile file)
               rel-dir     (str (java.util.UUID/randomUUID) "/")
               dest-file   (str (config/export-dir) rel-dir file-name)
               dest-dir    (str (config/export-dir) rel-dir)]
-          (handle-upload actual-file dest-file dest-dir file-name)
-          ;; (response/redirect (str "exports/" rel-dir "index.html"))
-          (response/redirect (str rel-dir "index.html"))))
+          (if (not (= test-field "42"))
+            (response/redirect "/")
+            (do
+              (handle-upload test-field actual-file dest-file dest-dir file-name)
+              (response/redirect (str rel-dir "index.html"))))))
   (route/resources "/")
   (route/not-found "404 error"))
 
